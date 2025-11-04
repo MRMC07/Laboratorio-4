50304b182114c4a27d426ec9787e61d8c6a97623
carlos0654 <deleonrodriguez888@gmail.com>
2025-11-04 08:13:57 -0600

    Parte vista resumen

--- Herramientas.java
+++ Herramientas.java
@@ -1,3 +1,15 @@
 public class Herramientas {
-    
+    private String tipo;
+
+    public Herramientas(String tipo){
+        //constructor
+        this.tipo = tipo;
+    }
+
+    public void aplicar(){
+
+    }
+    public void borrar(){
+        
+    }
 }
--- Investigacion.java
+++ Investigacion.java
@@ -1,4 +1,26 @@
+import java.util.ArrayList;
+
 public class Investigacion {
-    int id;
-    
+
+    private int id;
+    private String autor;
+    private int fecha;
+    private ArrayList<Tarjetas> tarjetas;
+
+    public Investigacion(int id, String nombre, int fecha){
+        //Constructor
+        this.id = id;
+        this.autor = nombre;
+        this.fecha = fecha;
+    }
+
+    public void  obtenerResumen(){
+
+    }
+
+    public void agregarEtiqueta(Etiquetas etiqueta){
+
+    }
+
+
 }
